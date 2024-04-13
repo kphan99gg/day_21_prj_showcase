49f969449ce449afc1be812614c3ec585f1be4cd
Khanh Phan <khanhp@amperecomputing.com>
2024-04-13 22:36:02 +0700

    Initial content in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+with col2:
+    st.title("Ardit Sulce")
+    content = '''
+    Hi, I am Ardit! I am a python programmer, teacher, and founder of PythonHow.
+    '''
+    st.info(content)
+
+content = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.subheader(content)
