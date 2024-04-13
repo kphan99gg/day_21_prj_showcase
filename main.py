75a76a4f005d7680980464fa785dc93a395dc187
Khanh Phan <khanhp@amperecomputing.com>
2024-04-13 23:37:18 +0700

    Add information of some projects

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
+
+data = pd.read_csv("data.csv", sep=';')
 
 col1, col2 = st.columns(2)
 
@@ -17,3 +20,18 @@ content = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.subheader(content)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+with col3:
+    for index, row in data[:10].iterrows():
+        st.title(row['title'])
+        st.info(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.title(row['title'])
+        st.info(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
